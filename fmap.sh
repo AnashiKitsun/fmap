12c75c2124ed7eb0ed3513df8e6874454760b005
Anashi <diamondguy9700@gmail.com>
2021-02-24 10:42:06 -0700

    Updating script for early testing on vm.

--- fmap.sh
+++ fmap.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
-# Get optional path argument
-path=$1
-if ! [[ $selector =~ ^[/.] ]] || [[ ! -z "$selector" ]]; then selector="."$selector; fi
-selector=".base"$(echo $path | /usr/bin/tr / .)
-echo $selector
+# Get path arg or default it to .
+path=${1:-.}
+# Now the selector will either be (/ or .), an unformatted selector, or a valid selection. (or invalid, but that will be handled elsewhere.)
+# We need to bring any of these to any selector formatted as .{selector}
+# Change all / to .
+selector=$(echo $path | /usr/bin/tr / .)
+# add . to start if it isn't there
+if ! [[ $selector =~ ^[/.] ]]; then selector="."$selector; fi
 
-# Start at specified path
-/usr/bin/cat $PWD/base.fmap | /usr/bin/jq $selector
-# /dev/null 2>&1
+echo Selection path: $selector
+
+# Start at specified path and get all values from selection
+selected_instructions=$(/usr/bin/jq -r $selector' | recurse | strings' $PWD/base.fmap)
+# loop through instructions and execute them
+echo "Instructions to run: \n ${selected_instructions[@]}"
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo Operation cancelled.
+  exit 0
+fi
+
+echo "Proceeding..."
+for instruction in ${selected_instructions[@]}
+do
+  echo "Executing instruction: $instruction"
+  # Split bits of data
+  IFS='|' read -ra instruction_set <<< "$instruction"
+  # Simplify which part is which for easier reference
+  source=${instruction_set[0]} # Copy from
+  dest=${instruction_set[1]} # Copy to
+  modx=${instruction_set[2]} # Chmod +x?
+  method=${instruction_set[3]} # Command to link it with? [IMPORTANT TO CHECK]
+  # Format data as necessary
+  source=$(echo $source | /usr/bin/sed 's/{ROOT}/'$(echo $PWD | /usr/bin/sed 's/\//\\\//g')'/g') # This weird little thing makes the working directory valid for /usr/bin/sed.
+  dest=$(echo $dest | /usr/bin/sed 's/{ROOT}/'$(echo $PWD | /usr/bin/sed 's/\//\\\//g')'/g')
+  modx=$(( $modx == 1 )) # Change int to bool
+  method=$(echo $method | /usr/bin/tr _ ' ') # Sub underscores for spaces
+  # Run and interpret the command
+  echo "Linking..."
+  #eval $method $source $dest
+  if [ $modx = 1 ]
+  then
+  #  chmod +x $dest
+    echo "$dest set as executable"
+  fi
+done
